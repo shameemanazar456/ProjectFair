eb149b16ed27fa3359cc0ae2082754a3d977a097
shameemanazar456 <shameemanazar456@gmail.com>
2024-06-10 16:31:52 +0530

    servereurl

--- src/services/baseUrl.js
+++ src/services/baseUrl.js
@@ -1,3 +1,4 @@
 //server address
 
-export const serverUrl = 'http://localhost:3000'
+//export const serverUrl = 'http://localhost:3000'
+export const serverUrl = 'https://projectfairbackend-mct3.onrender.com'
